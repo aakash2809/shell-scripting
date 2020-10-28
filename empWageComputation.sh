784ec7752066ba06a49b4b7e7ea09ae6d283cc68
aakash2809 <aakashrajak289@gmail.com>
2020-10-29 03:20:31 +0530

    Add : Computation using use case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -x
-
-wagePerHour=20
-fullTimeHour=8
-partTimeHour=4
-
 echo "Welcome to Employee Wage Computation"
-
+wagePerHour=20
+FULL_TIME_HOUR=8
+PART_TIME_HOUR=4
 #TO CHECK THE EMPLOYEE IS PRESENT OR ABSENT
 if [ $((RANDOM%2)) -eq 1 ]
 then
@@ -13,12 +10,16 @@ then
 else
 	echo "Employee is Absent"
 fi
-
-#TO CALCULATE FULL DAY EMPLOYEE DAILY WAGE
-calculateDailyWage=$(( wagePerHour*fullTimeHour))
-
-echo "full time employee daily wage - " $calculateDailyWage
-
-#TO CALCULATE PART TIME EMPLOYEE DAILY WAGE
-calculateDailyWage=$(( wagePerHour*partTimeHour))
-echo "part time employee daily wage - " $calculateDailyWage
+#TO CHECK EMPLOYEE IS PART TIME OR FULL TIME
+getWorkDonePerDay() {
+        case $1 in
+                0) workDonePerDay=$PART_TIME_HOUR;;
+                1) workDonePerDay=$FULL_TIME_HOUR;;
+                *) workDonePerDay=0;;
+        esac
+        echo $workDonePerDay
+}
+#TO CALCULATE EMPLOYEE DAILY WAGE
+workDonePerDay=$(getWorkDonePerDay $((RANDOM%2)) )
+calculateDailyWage=$(( wagePerHour*workDonePerDay ))
+echo "employee daily wage - " $calculateDailyWage
